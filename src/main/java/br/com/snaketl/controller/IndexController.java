100ab6640eea6d0c4e6d32c468b3e89cffd04597
Luiz Feltes <snaketl@gmail.com>
2014-05-31 18:53:26 -0300

    Primeiro commit

---
+++ src/main/java/br/com/snaketl/controller/IndexController.java
@@ -0,0 +1,16 @@
+package br.com.snaketl.controller;
+
+import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Get;
+
+/**
+ *
+ */
+@Controller
+public class IndexController {
+
+    @Get("/")
+    public void index() {
+    }
+
+}
